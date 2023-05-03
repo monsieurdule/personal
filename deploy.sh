0e09926c357d26369a4078345459f4db1dc6911f
monsieurdule <monsieurdule@gmail.com>
2023-05-03 12:58:48 +0200

    Security updates

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,35 @@
 #! /bin/bash
 
-# cd /home/qcerris/qcaas/frontend
-
+# source ~/personal/vars.config
 cp /home/qcerris/qcaas/constants.ts /home/qcerris/qcaas/frontend/src/utils/constants.ts
 
 # Build frontend and remove old build
+cd /home/qcerris/qcaas/frontend
 npm install
-npm run build
+# npm run build
 rm /home/qcerris/qcaas/frontend/build.tar.gz
 tar -cvzf /home/qcerris/qcaas/build.tar.gz /home/qcerris/qcaas/frontend/build
 
 # Copy build to server
-sshpass -p 'Qcerris2020!' scp /home/qcerris/qcaas/build.tar.gz dusan@qcaas.qcerris.net:/tmp
+sshpass -e scp /home/qcerris/qcaas/build.tar.gz dusan@qcaas.qcerris.net:/tmp
 
 # SSH to server and pull changes
-sshpass -p 'Qcerris2020!' ssh dusan@qcaas.qcerris.net \
-'echo Qcerris2020! | sudo -S ./pull_changes.sh;' \
-# Copy build to server
-'echo Qcerris2020! | sudo -S cp /tmp/build.tar.gz /var/www/hr/frontend' \
+sshpass -e ssh dusan@qcaas.qcerris.net \
+"echo $SSHPASS | sudo -S ./pull_changes.sh; \
+# Remove old backup and create new one
+echo $SSHPASS | sudo -S rm /var/www/hr/backend_test.tar.gz; \
+echo $SSHPASS | sudo -S tar -cvzf /var/www/hr/backend_test.tar.gz /var/www/hr/backend; \
+echo $SSHPASS | sudo -S rm /var/www/hr/frontend_test.tar.gz; \
+echo $SSHPASS | sudo -S tar -cvzf /var/www/hr/frontend_test.tar.gz /var/www/hr/frontend; \ 
+# Copy build to server and extract
+echo $SSHPASS | sudo -S cp /tmp/build.tar.gz /var/www/hr/frontend;" 
+# 'echo Qcerris2020! | sudo -S tar -xvzf /var/www/hr/frontend/build.tar.gz -C /var/www/hr/' \
 # Change settings.py
-'echo Qcerris2020! | sudo -S cp /var/www/hr/settings.py /var/www/hr/backend/performance_review/' \
+# 'echo Qcerris2020! | sudo -S cp /var/www/hr/settings.py /var/www/hr/backend/performance_review/' \
+# Make migrations
+# 'echo Qcerris2020! | sudo -S source venv/bin/activate'
+# cd /var/www/hr/backend
+# python3 manage.py makemigrations
 # Restart services
-'echo Qcerris2020! | sudo -S service qcaas restart' \
-'echo Qcerris2020! | sudo -S service nginx restart' \
+# 'echo Qcerris2020! | sudo -S service qcaas restart' \
+# 'echo Qcerris2020! | sudo -S service nginx restart' \
